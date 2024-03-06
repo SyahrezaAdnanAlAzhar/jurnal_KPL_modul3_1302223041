291f4610ed2c157ff759d4cc019796b62663ac94
SyahrezaAdnanAlAzhar <syahreza.adnan29@gmail.com>
2024-03-06 07:14:03 +0700

    penambahan button

---
+++ kalkulator_jurnal_KPL_modul3_1302223041/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kalkulator_jurnal_KPL_modul3_1302223041
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
